83fcbb4794e43e5d2d2c8f82a6e4839b67ac3980
Sumit Gupta <sumitsonu480@gmail.com>
2017-05-26 14:42:59 +0530

    Refreshed

---
+++ Collections.groovy
@@ -0,0 +1,14 @@
+def beatles=["John","Paul","Shyam","Rajesh"];
+for(int i=0;i<beatles.size();i++){
+def greeting="Hello, "
+println("\n")
+println("$greeting"+"${beatles[i]}");
+println"${i*10}\n"
+println'${i*10}\n'
+}
+//another way to print.
+println("\n")
+for(beatle in beatles){
+def gret="Hello, "
+println"$gret"+"$beatle"
+}
